5bda85fbbf8792f3d0cb542753d0e07c95f32f05
Khoa Nguyen <nguyenminhdangkhoa9@gmail.com>
2025-09-19 11:32:25 +0700

    add: add not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function NotFoundPage() {
+  return (
+    <div className="bg-background fixed inset-0 z-9999 flex flex-col items-center justify-center gap-12">
+      <h2 className="text-2xl md:text-4xl">404 | Page Not Found</h2>
+
+      <p className="text-base">{`The page you are looking for doesn't exist.`}</p>
+
+      <Link title="Back to HomePage" href="/" className="button outlined">
+        Back To HomePage
+      </Link>
+    </div>
+  );
+}
